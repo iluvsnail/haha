e5a9a0a4d3faa97638cda6d4c5fadba3661b4ba4
chenyl <cyl@gfire.cn>
2019-12-18 12:03:38 +0800

    add audit

--- src/main/scala/cn/smilegoo/hs/SwaggerConfig.scala
+++ src/main/scala/cn/smilegoo/hs/SwaggerConfig.scala
@@ -14,7 +14,7 @@ class SwaggerConfig {
 
   @Bean def api: Docket = new Docket(DocumentationType.SWAGGER_2)
     .select
-    .apis(RequestHandlerSelectors.basePackage("cn.gfire.gdp.cloud.resource.classifier"))
+    .apis(RequestHandlerSelectors.basePackage("cn.smilegoo.hs"))
     .paths(PathSelectors.any())
     .build
 
